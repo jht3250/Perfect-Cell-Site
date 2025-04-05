5767bddbf7675fd6f27af86d65727d4c3de9ca65
Kevin Zheng <kz3175@rit.edu>
2025-04-05 13:09:40 -0400

    fixed dropdown list widths

---
+++ src/assets/js/main.js
@@ -0,0 +1,14 @@
+const showDropdown = (e) => {
+    const dropdown = e.currentTarget;
+    const links = dropdown.querySelector('.links');
+
+    if (dropdown && links) {
+        const dropdownWidth = window.getComputedStyle(dropdown).width;
+        links.style.width = dropdownWidth;
+    }
+};
+
+let dropdowns = document.querySelectorAll('.dropdown');
+for (let dropdown of dropdowns) {
+    dropdown.addEventListener('mouseover', showDropdown);
+}
