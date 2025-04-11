729d26515710d0095b3443268dd2ddda6376b242
Kevin Zheng <kz3175@rit.edu>
2025-04-11 16:31:23 -0400

    fixed nav bar issue
    fixed the dropdown list width issue
    buttons in practice pages

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,16 +1,32 @@
-const showDropdown = (e) => {
-    const dropdown = e.currentTarget;
-    const links = dropdown.querySelector('.links');
+const updateDropdown = (ele, updateText) => {
+    ele.children[0].innerText = updateText;
+    ele.classList.toggle('expanded');
 
-    if (dropdown && links) {
-        setTimeout(() => {
-            const dropdownWidth = window.getComputedStyle(dropdown).width;
-            links.style.width = dropdownWidth;
-        }, 200);
-    }
-};
-
-let dropdowns = document.querySelectorAll('.dropdown');
-for (let dropdown of dropdowns) {
-    dropdown.addEventListener('mouseover', showDropdown);
+    const links = ele.querySelector('.links');
+    links.classList.toggle('active');
 }
+
+// Contrast Dropdown
+const contrast = document.querySelector('#expandC');
+contrast.addEventListener('mouseover', () => updateDropdown(contrast, 'Contrast'));
+contrast.addEventListener('mouseout', () => updateDropdown(contrast, 'C'));
+
+// Repetition Dropdown
+const repetition = document.querySelector('#expandR');
+repetition.addEventListener('mouseover', () => updateDropdown(repetition, 'Repetition'));
+repetition.addEventListener('mouseout', () => updateDropdown(repetition, 'R'));
+
+// Alignment Dropdown
+const alignment = document.querySelector('#expandA');
+alignment.addEventListener('mouseover', () => updateDropdown(alignment, 'Alignment'));
+alignment.addEventListener('mouseout', () => updateDropdown(alignment, 'A'));
+
+// Proximity Dropdown
+const proximity = document.querySelector('#expandP');
+proximity.addEventListener('mouseover', () => updateDropdown(proximity, 'Proximity'));
+proximity.addEventListener('mouseout', () => updateDropdown(proximity, 'P'));
+
+// Other Dropdown
+const other = document.querySelector('#others');
+other.addEventListener('mouseover', () => updateDropdown(other, '.'));
+other.addEventListener('mouseout', () => updateDropdown(other, '.'));
