e321859a689055781b5fa6d3349cf3eec223af5d
Kevin Zheng <kz3175@rit.edu>
2025-04-17 10:34:46 -0400

    comments

--- src/assets/js/interactive.js
+++ src/assets/js/interactive.js
@@ -4,8 +4,10 @@ for (let btn of correctBtns) {
         let badEx =  btn.parentElement.children[1];
         let goodEx =  btn.parentElement.children[2];
         
+        // Update text on buttons
         (badEx.classList.contains('display')) ? btn.innerText = 'See Bad Example Again' : btn.innerText = 'Correct This!';
 
+        // Toggle between bad and good examples
         badEx.classList.toggle('display');
         goodEx.classList.toggle('display');
     });
