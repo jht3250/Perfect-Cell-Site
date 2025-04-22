27d355d6c52fa76286ce78c18a5f7092e1548e13
Kevin Zheng <kz3175@rit.edu>
2025-04-22 11:24:25 -0400

    fixed nav js

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,35 +1,56 @@
+const removeAllDropdowns = () => {
+    const links = document.querySelectorAll('.links');
+    for (let link of links) {
+        link.parentElement.classList.remove('expanded');
+        link.classList.remove('active');
+    }
+};
+
 const updateDropdown = (ele, updateText) => {
-    // if (!ele.children[0].classList.contains('current')) ele.classList.remove('expanded');
+    removeAllDropdowns();
 
     const links = ele.querySelector('.links');
     links.classList.toggle('active');
 
     ele.classList.toggle('expanded');
-    // if (ele.children[0].classList.contains('current') && updateText.length === 1) return;
     ele.children[0].innerText = updateText;
-}
+};
 
 // Contrast Dropdown
 const contrast = document.querySelector('#expandC');
 contrast.addEventListener('mouseover', () => updateDropdown(contrast, 'Contrast'));
-contrast.addEventListener('mouseout', () => updateDropdown(contrast, 'C'));
+contrast.addEventListener('mouseout', () => {
+    updateDropdown(contrast, 'C');
+    removeAllDropdowns();
+});
 
 // Repetition Dropdown
 const repetition = document.querySelector('#expandR');
 repetition.addEventListener('mouseover', () => updateDropdown(repetition, 'Repetition'));
-repetition.addEventListener('mouseout', () => updateDropdown(repetition, 'R'));
+repetition.addEventListener('mouseout', () => {
+    updateDropdown(repetition, 'R');
+    removeAllDropdowns();
+});
 
 // Alignment Dropdown
 const alignment = document.querySelector('#expandA');
 alignment.addEventListener('mouseover', () => updateDropdown(alignment, 'Alignment'));
-alignment.addEventListener('mouseout', () => updateDropdown(alignment, 'A'));
-
+alignment.addEventListener('mouseout', () => {
+    updateDropdown(alignment, 'A');
+    removeAllDropdowns();
+});
 // Proximity Dropdown
 const proximity = document.querySelector('#expandP');
 proximity.addEventListener('mouseover', () => updateDropdown(proximity, 'Proximity'));
-proximity.addEventListener('mouseout', () => updateDropdown(proximity, 'P'));
+proximity.addEventListener('mouseout', () => {
+    updateDropdown(proximity, 'P');
+    removeAllDropdowns();
+});
 
 // Other Dropdown
 const other = document.querySelector('#others');
 other.addEventListener('mouseover', () => updateDropdown(other, '.'));
-other.addEventListener('mouseout', () => updateDropdown(other, '.'));
+other.addEventListener('mouseout', () => {
+    updateDropdown(other, '.');
+    removeAllDropdowns();
+});
