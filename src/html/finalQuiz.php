12e63cc07a19780a91f2f5895280464aa046f36b
Jake Hamilton <jameshamilton2743@gmail.com>
2025-04-22 11:24:14 -0400

    Final Quiz

--- src/html/finalQuiz.php
+++ src/html/finalQuiz.php
@@ -23,32 +23,112 @@
             <input type="text" id="A" name="A">
             <label for="P">P: </label>
             <input type="text" id="P" name="P">
-            
+            <br>
+            <br>
             <button id="Submit">Submit</button>
         </form>
     </div>
 
     <div class="question">
-        <p>What could happen if contrast is not properly implemented?</p>
+        <p>A web designer changes the background color of the page, then changes the color of the text to make it more legible. What principle is he using?</p>
         <form>
             <div>
-                <input type="radio" id="engagement" name="implement" value="incorrect">
-                <label for="engagement">The page becomes more engaging</label>
+                <input type="radio" id="contrast" name="implement" value="correct">
+                <label for="contrast">Contrast</label>
             </div>
             <br>
             <div>
-                <input type="radio" id="details" name="implement" value="correct">
-                <label for="details">Important Details could get lost or overlooked</label>
+                <input type="radio" id="repetition" name="implement" value="incorrect">
+                <label for="repetition">Repetition</label>
             </div>
             <br>
             <div>
-                <input type="radio" id="navigate" name="implement" value="incorrect">
-                <label for="navigate">Users can navigate the page easier</label>
+                <input type="radio" id="alignment" name="implement" value="incorrect">
+                <label for="alignment">Alignment</label>
             </div>
             <br>
             <div>
-                <input type="radio" id="brand" name="implement" value="incorrect">
-                <label for="brand">The brand identity becomes stronger</label>
+                <input type="radio" id="proximity" name="implement" value="incorrect">
+                <label for="proximity">Proximity</label>
             </div>
+        </form>
+    </div>
+
+    <div class="question">
+        <p>A lot of articles are stuffed together on a sidebar on a news website. This is a poor example of _____</p>
+        <form>
+            <div>
+                <input type="radio" id="contrast" name="implement" value="incorrect">
+                <label for="contrast">Contrast</label>
+            </div>
+            <br>
+            <div>
+                <input type="radio" id="repetition" name="implement" value="incorrect">
+                <label for="repetition">Repetition</label>
+            </div>
+            <br>
+            <div>
+                <input type="radio" id="alignment" name="implement" value="incorrect">
+                <label for="alignment">Alignment</label>
+            </div>
+            <br>
+            <div>
+                <input type="radio" id="proximity" name="implement" value="correct">
+                <label for="proximity">Proximity</label>
+            </div>
+        </form>
+    </div>
+
+    <div class="question">
+        <p>A man is writing an article about how much he hates his boss. Where should the header be?</p>
+        <form>
+            <div>
+                <input type="radio" id="left" name="implement" value="incorrect">
+                <label for="left">Left</label>
+            </div>
+            <br>
+            <div>
+                <input type="radio" id="right" name="implement" value="incorrect">
+                <label for="right">Right</label>
+            </div>
+            <br>
+            <div>
+                <input type="radio" id="center" name="implement" value="correct">
+                <label for="center">Center</label>
+            </div>
+            <br>
+            <div>
+                <input type="radio" id="box" name="implement" value="incorrect">
+                <label for="box">Box-Aligned</label>
+            </div>
+        </form>
+    </div>
+
+    <div class="question">
+        <p>What is the best way to utilize repetition?</p>
+        <form>
+            <div>
+                <input type="radio" id="same" name="implement" value="incorrect">
+                <label for="same">Strictly always the same alignments and set up to each page</label>
+            </div>
+            <br>
+            <div>
+                <input type="radio" id="skeleton" name="implement" value="correct">
+                <label for="skeleton">A general skeleton, but still enough to spice it up on each page</label>
+            </div>
+            <br>
+            <div>
+                <input type="radio" id="chaos" name="implement" value="incorrect">
+                <label for="chaos">Pure chaos</label>
+            </div>
+            <br>
+            <div>
+                <input type="radio" id="enough" name="implement" value="incorrect">
+                <label for="enough">Pretend to know what you are doing enough</label>
+            </div>
+        </form>
+    </div>
+    
+
 </main>
 <?php require_once($path . 'src/assets/inc/footer.inc.php'); ?>
