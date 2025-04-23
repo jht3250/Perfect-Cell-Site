71a6134d61f0e1e1e3e9fd03f9b7375256025828
Kevin Zheng <kz3175@rit.edu>
2025-04-23 19:27:47 -0400

    threw in the code pen stuff
    and restyled

--- src/assets/inc/codeTest.inc.php
+++ src/assets/inc/codeTest.inc.php
@@ -1,12 +1,26 @@
 <main>
     <h1>Try it yourself!</h1>
 
-    <section class="codepen">
-        <!-- Code pen stuff here! -->
-        codepen
+    <section class="container">
+        <!-- <div class="editor"> -->
+            <div class="left">
+                <label for="html-code">HTML</label>
+                <textarea onkeyup="run()" spellcheck="false" id="html-code" rows="20" cols="30"></textarea>
+
+                <label for="css-code"></i>CSS</label>
+                <textarea onkeyup="run()" spellcheck="false" id="css-code" rows="15" cols="30"></textarea>
+
+                <label for="js-code">JavaScript</label>
+                <textarea onkeyup="run()" spellcheck="false" id="js-code" rows="10" cols="30"></textarea>
+            </div>
+            <div class="right">
+                <label>Output</label>
+                <iframe id="output"></iframe>
+            </div>
+        <!-- </div> -->
     </section>
 
-    <p>This Feature Is Only Available On Desktop Version : &#40;</p>
+    <p class="mobile">This Feature Is Only Available On Desktop Version : &#40;</p>
 
     <a href="<?php echo $quizFile; ?>" class="button">Take Quiz</a>
 </main>
